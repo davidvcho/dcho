163d3c0c188a628b08bec60ce63b35f03a4e53d0
David Cho <david@peachstreet.com>
2021-01-24 21:55:59 -0800

    Get rid of header bottom border

--- web/src/common/components/base/navigation/header-navigation/header-navigation.tsx
+++ web/src/common/components/base/navigation/header-navigation/header-navigation.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import {
   HeaderNavigation as BaseHeaderNavigation,
+  HeaderNavigationProps as BaseHeaderNavigationProps,
   StyledNavigationItem as BaseNavigationItem,
   StyledNavigationList as BaseNavigationList,
   ALIGN as BASE_ALIGN,
 } from "baseui/header-navigation";
 
-export const HeaderNavigation = BaseHeaderNavigation;
+export const HeaderNavigation: React.FC<BaseHeaderNavigationProps> = (props) => {
+  return <BaseHeaderNavigation  {...props} overrides={{Root: {
+      style: () => ({
+        "border-bottom-style": "none",
+      }),}
+  }} />;
+};
 
 export const NavigationList = BaseNavigationList;
 

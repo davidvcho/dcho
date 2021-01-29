af7141fbde1f2719f90b011d1ca307b9351b9cc2
David Cho <david@peachstreet.com>
2021-01-28 16:10:28 -0800

    Route home logo to about for now

--- web/src/pages/home/home-page.tsx
+++ web/src/pages/home/home-page.tsx
@@ -3,11 +3,12 @@ import { Logo } from "pages/home/logo";
 import { Link } from "common/components/base/navigation/link/link";
 import { CenteredBlock } from "common/components/layout/block/centered-block";
 import styles from "pages/home/home-page.module.scss";
+import { Pages } from "pages/router";
 
 export const HomePage: React.FC = () => {
   return (
     <CenteredBlock marginTop="15vh">
-      <Link href={"https://www.google.com"}>
+      <Link href={Pages.ABOUT}>
         <Logo className={styles.logo} color="black" />
       </Link>
     </CenteredBlock>
--- web/src/pages/router.tsx
+++ web/src/pages/router.tsx
@@ -4,10 +4,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomePage } from "pages/home/home-page";
 import { AboutPage } from "pages/about/about-page";
 
+export enum Pages {
+  ABOUT = "/me",
+  HOME = "/",
+}
+
 export const Router: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/me" component={AboutPage} />
+      <Route path={Pages.ABOUT} component={AboutPage} />
+      {/* This is always the default page. */}
       <Route component={HomePage} />
     </Switch>
   </BrowserRouter>

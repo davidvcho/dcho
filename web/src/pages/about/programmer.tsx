75a23b7ec23d08b95e7262288e1f4d582284b5b3
David Cho <david@peachstreet.com>
2021-01-31 20:54:59 -0500

    Add programmer experience and make it responsive

--- web/src/pages/about/programmer.tsx
+++ web/src/pages/about/programmer.tsx
@@ -1,5 +1,70 @@
 import React from "react";
+import { Experience, experiences } from "pages/about/experiences";
+import { Heading, HeadingLevel } from "common/components/content/heading";
+import {
+  LabelSmall,
+  ParagraphMedium,
+} from "common/components/content/typography";
+import { Block } from "common/components/layout/block/block";
+
+const ExperienceInfo: React.FC<{ experience: Experience }> = ({
+  experience,
+}) => {
+  const logo = (
+    <Block
+      alignItems="center"
+      display="flex"
+      height={["50px", "80px"]}
+      minWidth={["50px", "80px"]}
+      width={["50px", "80px"]}
+    >
+      <img
+        alt={experience.organization.name}
+        height="auto"
+        src={experience.organization.logoImagePath}
+        style={{ maxWidth: "100%" }}
+      />
+    </Block>
+  );
+
+  const header = (
+    <div>
+      <Heading margin={0}>{experience.organization.name}</Heading>
+      <LabelSmall>
+        {experience.timeframe.start} — {experience.timeframe.end}
+      </LabelSmall>
+    </div>
+  );
+
+  const responsiveHeader = (
+    <>
+      <Block alignItems="center" display={["flex", "none"]} gridGap="20px">
+        {logo}
+        {header}
+      </Block>
+      <Block display={["none", "block"]}>{header}</Block>
+    </>
+  );
+
+  return (
+    <HeadingLevel>
+      <Block display="flex" gridGap="30px">
+        <Block display={["none", "block"]}>{logo}</Block>
+        <Block>
+          {responsiveHeader}
+          <ParagraphMedium>{experience.blurb}</ParagraphMedium>
+        </Block>
+      </Block>
+    </HeadingLevel>
+  );
+};
 
 export const Programmer: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      {experiences.map((experience: Experience) => (
+        <ExperienceInfo experience={experience} />
+      ))}
+    </>
+  );
 };
